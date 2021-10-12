4cd6ba7e196abfd539ef11c59c1f6ea7f97216bb
Junho Yeo <i@junho.io>
2021-10-13 04:40:15 +0900

    Upload manifest to Arweave

--- src/index.ts
+++ src/index.ts
@@ -24,13 +24,34 @@ const uploadMetadata = async () => {
     isChunked: true,
   });
 
-  await testWeave.mine();
-
   const imageUri = getTransactionUri({
     connection: arweave,
     transaction: imageTransaction,
   });
   console.log({ imageUri });
+
+  const manifest = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, '../assets/nft.json')).toString(),
+  );
+  manifest.image = imageUri;
+  manifest.properties.files[0].uri = imageUri;
+  manifest.properties.files[0].type = 'image/png';
+
+  const manifestTransaction = await uploadData({
+    connection: arweave,
+    data: JSON.stringify(manifest),
+    contentType: 'application/json',
+    jwk,
+    isChunked: false,
+  });
+
+  const manifestUri = getTransactionUri({
+    connection: arweave,
+    transaction: manifestTransaction,
+  });
+  console.log({ manifestUri });
+
+  await testWeave.mine();
 };
 
 uploadMetadata();
